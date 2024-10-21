b6e2a3195eda46cea1a3314801a4473303db6a9d
Belle Sangthong <belle.sangthong@iths.se>
2024-10-21 12:42:16 +0200

    make a test when a recipe added

---
+++ RecipeManagement/src/Recipe.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Recipe {
+    private String name;
+    private List<String> ingredients;
+    private String instructions;
+
+    public Recipe(String name, List<String> ingredients, String instructions) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- RecipeManagement/test/RecipeTest.java
+++ RecipeManagement/test/RecipeTest.java
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
 
 public class RecipeTest {
 
     @Test
     void testAddRecipe() {
         RecipeBook recipeBook = new RecipeBook();
+        Recipe recipe = new Recipe("Pancakes", Arrays.asList("Flour", "Milke", "Eggs"), "Mix and cook.");
+
+        assertEquals("Pancakes", recipe.getName());
+
+
 
 
     }
